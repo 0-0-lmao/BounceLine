66267b66785c843f18e70a3dc679ece8f688d85d
( ._.) <jacob.hoge@trojans.dsu.edu>
2022-10-03 14:55:47 -0500

    box not bounce correct

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class CubeMove : MonoBehaviour
 {
     public float scaler;
+    public AnimationCurve animCurve;
+    private bool bounce;
+    private Vector3 orginalPos;
 
+    private void Start()
+    {
+        orginalPos = transform.position;
+    }
     private void OnTriggerEnter(Collider Other)
     {
         Debug.Log("Hit");
-        transform.position = new Vector3(transform.position.x, transform.position.y + scaler, transform.position.z);
-        transform.position = new Vector3(transform.position.x, transform.position.y - scaler, transform.position.z);
-
+        scaler = 0f;
+        bounce = true;
+    }
+    private void Update()
+    {
+        /*if (bounce)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + animCurve.Evaluate(scaler), transform.position.z);
+            scaler += 0.25f * Time.deltaTime;
+            if (scaler >= 1f)
+            {
+                Debug.Log("Fortnite");
+                scaler = 0f;
+                transform.position = orginalPos;
+                bounce = false;
+            }
+        }
+        */
 
     }
 }
